e4d243f859e890e5919b351ca4f6ecfc862068af
Phong Ha <realphongha@gmail.com>
2023-11-04 19:38:03 +0700

    Add a little comment

--- luas/basic.lua
+++ luas/basic.lua
@@ -234,6 +234,8 @@ vim.api.nvim_set_keymap("v", "J", [[:m '>+1<CR>gv=gv]],
 vim.api.nvim_set_keymap("v", "K", [[:m '<-2<CR>gv=gv]],
     {noremap = true, silent = true}
 )
+
+-- using Ctrl+j, Ctrl+j for navigating in command mode
 vim.api.nvim_set_keymap("c", "<C-k>", "<Up>",
     {noremap = true, silent = true}
 )
