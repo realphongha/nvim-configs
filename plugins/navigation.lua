d68ab597c0738ed49ee1454286fa21b80e6c1306
Phong Ha <realphongha@gmail.com>
2024-01-17 09:07:51 +0700

    change finding in all files to leader+a for telescope

--- plugins/navigation.lua
+++ plugins/navigation.lua
@@ -5,7 +5,7 @@ return {
         'nvim-telescope/telescope.nvim', branch = '0.1.x',
         lazy = true,
         keys = {
-            {"<C-p>a", mode = "n"},
+            {"<leader>a", mode = "n"},
             {"<C-p>", mode = "n"},
             {"<leader>g", mode = "n"},
             {"<leader>b", mode = "n"},
@@ -13,7 +13,7 @@ return {
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function ()
             local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<C-p>a', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>a', builtin.find_files, {})
             vim.keymap.set('n', '<C-p>', builtin.git_files, {})
             vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
             vim.keymap.set('n', '<leader>b', builtin.buffers, {})
