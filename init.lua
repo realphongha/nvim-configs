e3f8f47f076db859bacf9f6f658b259971e5b79f
Phong Ha <realphongha@gmail.com>
2024-04-14 11:09:45 +0700

    better looking transparent

--- init.lua
+++ init.lua
@@ -170,15 +170,13 @@ function SeeWaifu(color)
     --     'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
     --     'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
     --     'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
-    --     'EndOfBuffer', "NvimTreeNormal", "TelescopeNormal"
-    --     'Pmenu', 'NormalFloat', 'FloatShadow', "NvimTreePopup"
+    --     'EndOfBuffer',
+    --     "NvimTreeNormal", "NvimTreeNormalNC", "NvimTreePopup", -- nvim-tree
+    --     "TelescopeNormal", -- telescope.nvim
+    --     'Pmenu', 'NormalFloat', 'FloatShadow', 
     -- }
     local hi_groups = {
-        'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
-        'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
-        'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
-        'SignColumn', 'CursorLine', 'CursorLineNr', 'StatusLine', 'StatusLineNC',
-        'EndOfBuffer',
+        'Normal', 'NormalNC',
         "NvimTreeNormal", "NvimTreeNormalNC", -- nvim-tree
         "TelescopeNormal" -- telescope.nvim
     }
