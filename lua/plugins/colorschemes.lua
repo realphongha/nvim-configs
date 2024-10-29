bfedf7272332ddec7009760dd791ae142afbfff0
Phong Ha <realphongha@gmail.com>
2024-10-29 10:08:44 +0700

    add no-clown-fiesta colorscheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -2,28 +2,29 @@ return {
     --------------------------------------------------------------------------
     -- {{{ catppuccin
     {
-        "catppuccin/nvim", name = "catppuccin",
+        "catppuccin/nvim",
+        name = "catppuccin",
         lazy = true,
         build = ":CatppuccinCompile",
         opts = {
             flavour = "mocha", -- latte, frappe, macchiato, mocha
-            background = { -- :h background
+            background = {     -- :h background
                 light = "latte",
                 dark = "mocha",
             },
             transparent_background = false, -- disables setting the background color.
-            show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-            term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+            show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+            term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
             dim_inactive = {
-                enabled = false, -- dims the background color of inactive window
+                enabled = false,            -- dims the background color of inactive window
                 shade = "dark",
-                percentage = 0.15, -- percentage of the shade to apply to the inactive window
+                percentage = 0.15,          -- percentage of the shade to apply to the inactive window
             },
-            no_italic = false, -- Force no italic
-            no_bold = false, -- Force no bold
-            no_underline = false, -- Force no underline
-            styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-                comments = { "italic" }, -- Change the style of comments
+            no_italic = false,              -- Force no italic
+            no_bold = false,                -- Force no bold
+            no_underline = false,           -- Force no underline
+            styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
+                comments = { "italic" },    -- Change the style of comments
                 conditionals = { "italic" },
                 loops = {},
                 functions = {},
@@ -69,7 +70,8 @@ return {
     --------------------------------------------------------------------------
     -- {{{ rose-pine
     {
-        "rose-pine/neovim", name = "rose-pine",
+        "rose-pine/neovim",
+        name = "rose-pine",
         lazy = true,
         opts = {
             --- @usage 'auto'|'main'|'moon'|'dawn'
@@ -103,14 +105,15 @@ return {
     --------------------------------------------------------------------------
     -- {{{ tokyonight
     {
-        "folke/tokyonight.nvim", name = "tokyonight",
+        "folke/tokyonight.nvim",
+        name = "tokyonight",
         lazy = true,
         opts = {
             -- your configuration comes here
             -- or leave it empty to use the default settings
-            style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-            light_style = "day", -- The theme is used when the background is set to light
-            transparent = false, -- Enable this to disable setting the background color
+            style = "storm",        -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+            light_style = "day",    -- The theme is used when the background is set to light
+            transparent = false,    -- Enable this to disable setting the background color
             terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
             styles = {
                 -- Style to be applied to different syntax groups
@@ -120,14 +123,14 @@ return {
                 functions = {},
                 variables = {},
                 -- Background styles. Can be "dark", "transparent" or "normal"
-                sidebars = "dark", -- style for sidebars, see below
-                floats = "dark", -- style for floating windows
+                sidebars = "dark",            -- style for sidebars, see below
+                floats = "dark",              -- style for floating windows
             },
-            sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-            day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+            sidebars = { "qf", "help" },      -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+            day_brightness = 0.3,             -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
             hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-            dim_inactive = false, -- dims inactive windows
-            lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+            dim_inactive = false,             -- dims inactive windows
+            lualine_bold = false,             -- When `true`, section headers in the lualine theme will be bold
 
             --- You can override specific color groups to use other groups or a hex color
             --- function will be called with a ColorScheme table
@@ -146,30 +149,31 @@ return {
     --------------------------------------------------------------------------
     -- {{{ kanagawa.nvim
     {
-        "rebelot/kanagawa.nvim", name = "kanagawa",
+        "rebelot/kanagawa.nvim",
+        name = "kanagawa",
         lazy = true,
         build = ":KanagawaCompile",
         opts = {
-            compile = true,              -- enable compiling the colorscheme
-            undercurl = true,            -- enable undercurls
+            compile = true,   -- enable compiling the colorscheme
+            undercurl = true, -- enable undercurls
             commentStyle = { italic = true },
             functionStyle = {},
-            keywordStyle = { italic = true},
+            keywordStyle = { italic = true },
             statementStyle = { bold = true },
             typeStyle = {},
-            transparent = false,         -- do not set background color
-            dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
-            terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-            colors = {                   -- add/modify theme and palette colors
+            transparent = false,   -- do not set background color
+            dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+            terminalColors = true, -- define vim.g.terminal_color_{0,17}
+            colors = {             -- add/modify theme and palette colors
                 palette = {},
                 theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
             },
             overrides = function(colors) -- add/modify highlights
                 return {}
             end,
-            theme = "wave",              -- Load "wave" theme when 'background' option is not set
-            background = {               -- map the value of 'background' option to a theme
-                dark = "wave",           -- try "dragon" !
+            theme = "wave",    -- Load "wave" theme when 'background' option is not set
+            background = {     -- map the value of 'background' option to a theme
+                dark = "wave", -- try "dragon" !
                 light = "lotus"
             },
         }
@@ -177,9 +181,30 @@ return {
     -- }}}
 
     --------------------------------------------------------------------------
+    -- {{{ no-clown-fiesta.nvim
+    {
+        "aktersnurra/no-clown-fiesta.nvim",
+        name = "no-clown-fiesta",
+        lazy = true,
+        config = function()
+            local plugin = require "no-clown-fiesta"
+            plugin.setup({
+                styles = {
+                    type = { bold = true },
+                    lsp = { underline = false },
+                    match_paren = { underline = true },
+                },
+            })
+            return plugin.load()
+        end
+    },
+    -- }}}
+
+    --------------------------------------------------------------------------
     -- {{{ gruvbox
     {
-        "morhetz/gruvbox", name = "gruvbox",
+        "morhetz/gruvbox",
+        name = "gruvbox",
         lazy = true,
     },
     -- }}}
@@ -187,7 +212,8 @@ return {
     --------------------------------------------------------------------------
     -- {{{ monokai
     {
-        "tanvirtin/monokai.nvim", name = "monokai",
+        "tanvirtin/monokai.nvim",
+        name = "monokai",
         lazy = true,
     },
     -- }}}
@@ -195,7 +221,8 @@ return {
     --------------------------------------------------------------------------
     -- {{{ peaksea
     {
-        "calincru/peaksea.vim", name = "peaksea",
+        "calincru/peaksea.vim",
+        name = "peaksea",
         lazy = true,
     },
     -- }}}
@@ -203,7 +230,8 @@ return {
     --------------------------------------------------------------------------
     -- {{{ solarized
     {
-        "altercation/vim-colors-solarized", name = "solarized",
+        "altercation/vim-colors-solarized",
+        name = "solarized",
         lazy = true,
     },
     -- }}}
