4f07905a0efa915087387ee497edada290743b45
Phong Ha <realphongha@gmail.com>
2025-01-14 14:20:13 +0700

    [add] ts-comments

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -29,6 +29,15 @@ return {
     },
     -- }}}
 
+    -- {{{ ts-comments
+    {
+      "folke/ts-comments.nvim",
+      opts = {},
+      event = "VeryLazy",
+      enabled = vim.fn.has("nvim-0.10.0") == 1,
+    },
+    -- }}}
+
     --------------------------------------------------------------------------
     -- {{{ glow.nvim
     {
