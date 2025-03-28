ba7324c0218745e221082ec8c6c948d88530791e
Phong Ha <realphongha@gmail.com>
2025-03-28 10:26:21 +0700

    add github copilot support

--- lua/plugins/llm.lua
+++ lua/plugins/llm.lua
@@ -1,5 +1,59 @@
 return {
     --------------------------------------------------------------------------
+    -- {{{ copilot.vim
+    {
+        "github/copilot.vim",
+        lazy = true,
+        cmd = "Copilot",
+        keys = {
+            { "<C-c>", mode = { "i" } },
+            { "<C-f>", mode = { "i" } },
+            { "<C-g>", mode = { "i" } }
+        },
+        config = function()
+            vim.keymap.set('i', '<C-c>',
+                'copilot#Accept("\\<CR>")',
+                { expr = true, silent = true, noremap = true, replace_keycodes = false })
+            vim.keymap.set('i', '<C-f>',
+                'copilot#Previous()',
+                { expr = true, silent = true, noremap = true })
+            vim.keymap.set('i', '<C-g>',
+                'copilot#Next()',
+                { expr = true, silent = true, noremap = true })
+            vim.g.copilot_no_tab_map = true
+        end
+    },
+    -- }}}
+
+    --------------------------------------------------------------------------
+    -- {{{ CopilotChat.nvim
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        lazy = false,
+        keys = {
+            { "<leader>cc", mode = { "n", "v" } },
+        },
+        cmd = "CopilotChat",
+        dependencies = {
+            { "github/copilot.vim" },                 -- or zbirenbaum/copilot.lua
+            { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+        },
+        build = "make tiktoken",                      -- Only on MacOS or Linux
+        -- opts = {
+        --     model = "claude-3.5-sonnet",
+        -- },
+        config = function()
+            require("CopilotChat").setup({
+                model = "claude-3.5-sonnet",
+            })
+            require("which-key").add({
+                { "<leader>cc", ":CopilotChat<CR>", desc = "Open Copilot Chat" },
+            })
+        end
+    },
+    -- }}}
+
+    --------------------------------------------------------------------------
     -- {{{ codeium.vim
     {
         'Exafunction/codeium.vim',
@@ -45,10 +99,12 @@ return {
             require("codecompanion").setup({
                 strategies = {
                     chat = {
-                        adapter = "ollama_deepseek_r1",
+                        -- adapter = "ollama_deepseek_r1",
+                        adapter = "copilot",
                     },
                     inline = {
-                        adapter = "ollama_deepseek_r1",
+                        -- adapter = "ollama_deepseek_r1",
+                        adapter = "copilot",
                     },
                 },
                 adapters = {
