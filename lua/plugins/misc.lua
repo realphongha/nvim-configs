fc880ce9bc4daa73b3b5f8346c8cd558ff9830f3
Phong Ha <realphongha@gmail.com>
2025-12-03 11:56:37 +0700

    tweak leetcode plugin

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -92,30 +92,8 @@ return {
             "nvim-tree/nvim-web-devicons",
         },
         opts = {
-            ---@type string
             arg = leet_arg,
-
-            ---@type lc.lang
             lang = "python3", -- cpp, python3
-
-            cn = {            -- leetcode.cn
-                enabled = false, ---@type boolean
-                translator = true, ---@type boolean
-                translate_problems = true, ---@type boolean
-            },
-
-            ---@type lc.storage
-            storage = {
-                home = vim.fn.stdpath("data") .. "/leetcode",
-                cache = vim.fn.stdpath("cache") .. "/leetcode",
-            },
-
-            ---@type table<string, boolean>
-            plugins = {
-                non_standalone = false,
-            },
-
-            ---@type boolean
             logging = true,
 
             injector = {
@@ -150,68 +128,10 @@ return {
                         "",
                     }
                 }
-            }, ---@type table<lc.lang, lc.inject>
-
-            cache = {
-                update_interval = 60 * 60 * 24 * 7, ---@type integer 7 days
             },
-
-            console = {
-                open_on_runcode = true, ---@type boolean
-
-                dir = "row", ---@type lc.direction
-
-                size = { ---@type lc.size
-                    width = "90%",
-                    height = "75%",
-                },
-
-                result = {
-                    size = "60%", ---@type lc.size
-                },
-
-                testcase = {
-                    virt_text = true, ---@type boolean
-
-                    size = "40%", ---@type lc.size
-                },
-            },
-
-            description = {
-                position = "left", ---@type lc.position
-
-                width = "40%", ---@type lc.size
-
-                show_stats = true, ---@type boolean
-            },
-
-            hooks = {
-                ---@type fun()[]
-                ["enter"] = {},
-
-                ---@type fun(question: lc.ui.Question)[]
-                ["question_enter"] = {},
-
-                ---@type fun()[]
-                ["leave"] = {},
-            },
-
-            keys = {
-                toggle = { "q", "<Esc>" }, ---@type string|string[]
-                confirm = { "<CR>" }, ---@type string|string[]
-
-                reset_testcases = "r", ---@type string
-                use_testcase = "U", ---@type string
-                focus_testcases = "H", ---@type string
-                focus_result = "L", ---@type string
-            },
-
-            ---@type boolean
-            image_support = false,
+            image_support = true,
+            picker = { provider = "telescope" },
         },
-        -- config = function ()
-        --     vim.cmd("colorscheme catppuccin")
-        -- end
     }
     --}}}
 }
