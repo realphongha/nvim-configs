2c8f41698a3477ba6033d8fa57bd44bab2312393
Phong Ha <realphongha@gmail.com>
2025-12-10 10:24:22 +0700

    refactor

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -382,9 +382,6 @@ return {
                     -- REQUIRED - you must specify a snippet engine
                     expand = function(args)
                         vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-                        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-                        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
                     end,
                 },
                 window = {
@@ -582,25 +579,17 @@ return {
             local configs = require("nvim-treesitter.configs")
 
             configs.setup({
-                -- A list of parser names, or "all" (the listed parsers should always be installed)
                 ensure_installed = { "c", "lua", "vim", "vimdoc", "python", "cmake", "markdown" },
-                -- Install parsers synchronously (only applied to `ensure_installed`)
                 sync_install = false,
                 -- Automatically install missing parsers when entering buffer
                 -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
                 auto_install = true,
-                -- List of parsers to ignore installing (for "all")
                 -- ignore_install = { "javascript" },
-                ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-                -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
                 indent = {
                     enable = true,
                 },
                 highlight = {
                     enable = true,
-                    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-                    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-                    -- the name of the parser)
                     -- list of language that will be disabled
                     -- disable = { "c", "rust" },
                     -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
@@ -611,10 +600,6 @@ return {
                             return true
                         end
                     end,
-                    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                    -- Instead of true it can also be a list of languages
                     additional_vim_regex_highlighting = false,
                 },
             })
@@ -632,43 +617,19 @@ return {
                 textobjects = {
                     select = {
                         enable = true,
-
-                        -- Automatically jump forward to textobj, similar to targets.vim
                         lookahead = true,
-
                         keymaps = {
-                            -- disable it due to conflict with which-key
-                            -- You can use the capture groups defined in textobjects.scm
                             ["af"] = "@function.outer",
                             ["if"] = "@function.inner",
                             ["ac"] = "@class.outer",
-                            -- You can optionally set descriptions to the mappings (used in the desc parameter of
-                            -- nvim_buf_set_keymap) which plugins like which-key display
-                            -- ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-                            -- You can also use captures from other query groups like `locals.scm`
+                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
                             -- ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
                         },
-                        -- You can choose the select mode (default is charwise 'v')
-                        --
-                        -- Can also be a function which gets passed a table with the keys
-                        -- * query_string: eg '@function.inner'
-                        -- * method: eg 'v' or 'o'
-                        -- and should return the mode ('v', 'V', or '<c-v>') or a table
-                        -- mapping query_strings to modes.
                         selection_modes = {
                             ['@parameter.outer'] = 'v', -- charwise
                             ['@function.outer'] = 'V', -- linewise
                             ['@class.outer'] = '<c-v>', -- blockwise
                         },
-                        -- If you set this to `true` (default is `false`) then any textobject is
-                        -- extended to include preceding or succeeding whitespace. Succeeding
-                        -- whitespace has priority in order to act similarly to eg the built-in
-                        -- `ap`.
-                        --
-                        -- Can also be a function which gets passed a table with the keys
-                        -- * query_string: eg '@function.inner'
-                        -- * selection_mode: eg 'v'
-                        -- and should return true or false
                         -- include_surrounding_whitespace = true,
                     },
                 },
