0bc56ba96be4a38f084ac7f670f4aadc03266c2b
Phong Ha <realphongha@gmail.com>
2025-12-10 10:09:25 +0700

    remove netrw configs

--- init.lua
+++ init.lua
@@ -402,32 +402,6 @@ vim.cmd([[
 " Disables netrw by default
 let g:loaded_netrw=1
 let g:loaded_netrwPlugin=1
-
-" Hit enter in the file browser to open the selected
-" file with :vsplit to the right of the browser.
-let g:netrw_browse_split=4
-let g:netrw_altv=1
-
-" Default to tree mode
-let g:netrw_liststyle=3
-
-" Keep the current directory and the browsing directory synced
-let g:netrw_keepdir=0
-
-" Window size
-let g:netrw_winsize=20
-
-" Hide banner
-let g:netrw_banner=0
-
-" Enable recursive copy
-let g:netrw_localcopydircmd='cp -r'
-
-" Sort is affecting only: directories on the top, files below
-let g:netrw_sort_sequence = '[\/]$,*'
-
-" Highlight marked files
-hi! link netrwMarkFile Search
 ]])
 
 -- the silver searcher
