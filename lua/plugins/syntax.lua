e7ce2dc59095333e4922c07c15567f72611d44dc
Phong Ha <realphongha@gmail.com>
2025-12-22 14:42:13 +0700

    use linewise for class object in nvim-treesitter-textobjects

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -628,7 +628,7 @@ return {
                         selection_modes = {
                             ['@parameter.outer'] = 'v', -- charwise
                             ['@function.outer'] = 'V', -- linewise
-                            ['@class.outer'] = '<c-v>', -- blockwise
+                            ['@class.outer'] = 'V', -- linewise
                         },
                         -- include_surrounding_whitespace = true,
                     },
